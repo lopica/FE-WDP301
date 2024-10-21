6e21a656e0e2fcea477c666b33617af20fd4b7d0
Ha Gia Manh <manhhacvp@gmail.com>
2024-10-21 23:03:26 +0700

    LY home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/WEARIT.svg";
 
 const Footer = () => {
   return (
-    <footer className="bg-white mt-40">
+    <footer className="bg-white mt-40 py-10 border-t-2 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center mb-8">
           <div className="flex justify-center md:justify-start items-center mb-4 md:mb-0">
@@ -47,7 +47,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="mt-8 flex justify-center md:justify-between items-center">
-          <p className="text-gray-600 text-sm">© 2024 HolaWear. All rights reserved Checking</p>
+          <p className="text-gray-600 text-sm">© 2024 HolaWear. All rights reserved</p>
         </div>
       </div>
     </footer>
